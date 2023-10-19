1edef298573468719104abeb9fc576428dd78d99
Hasti-babaei <beingbabaei@gmail.com>
2023-10-19 12:22:57 +0330

    modify codes

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,24 +1,11 @@
-let Username=document.getElementById('Username').value;
-let Password=document.getElementById('Password').value;
-let btnLogin=document.getElementById('login');
+let Username = document.getElementById("Username").value;
+let Password = document.getElementById("Password").value;
+let btnLogin = document.getElementById("login");
 
-btnLogin.addEventListener('click', function(){
-
-    if (Password == "" ) {
-        alert("Name must be filled out");
-
-    }
-    else if( Password.lenght<=5){
-        alert("There are not enough characters");
-            }
-            else if (Username == "" ) {
-                alert("Name must be filled out");
-            }
-            else if( Username.lenght<=5)
-                alert("There are not enough characters");
-                else{
-                    location.replace('pages/home/home.html');
-                }
-                    
-})
-   
+function goHome() {
+  if (Password == "" || Username == "") {
+    alert("Name must be filled out");
+  } else {
+    location.replace("/pages/home/home.html");
+  }
+}
