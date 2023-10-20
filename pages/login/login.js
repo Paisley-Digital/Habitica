b16bc4522e7e8898875f01398f7ae6833684f6eb
hamidshariat <hamid13841147@gmail.com>
2023-10-20 17:30:47 +0330

    Add button and save

--- pages/login/login.js
+++ pages/login/login.js
@@ -4,21 +4,20 @@ let btnLogin=document.getElementById('login');
 
 btnLogin.addEventListener('click', function(){
 
-    if (Password == "" ) {
+    if (Password === "" ) {
         alert("Name must be filled out");
-
+    }
+    else if (Username === "" ) {
+        alert("Name must be filled out");
     }
     else if( Password.lenght<=5){
         alert("There are not enough characters");
             }
-            else if (Username == "" ) {
-                alert("Name must be filled out");
-            }
-            else if( Username.lenght<=5)
+            else if( Username.lenght<=5){
                 alert("There are not enough characters");
-                else{
+            }
                     location.replace('/pages/home/home.html');
-                }
+                
                     
 })
    
